3f73dd1ff045700c02d98fde5e549569e111bf55
Weis <772896304@qq.com>
2018-04-01 19:36:04 +0800

    add_test

---
+++ test/test/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<cctype>
+#include<sstream>
+#include<string>
+#include<cstring>
+#include<deque>
+#include<map>
+#include<set>
+#include<vector>
+#include<list>
+#include<cmath>
+#include<ctime>
+#include<cstdlib>
+#include<fstream>
+#define sf(a) scanf("%lf",&a)
+#define sd1(a) scanf("%d",&a)
+#define sd2(a,b) scanf("%d%d",&a,&b)
+#define sd3(a,b,c) scanf("%d%d%d",&a,&b,&c)
+#define sf(a) scanf("%lf",&a)
+#define sf2(a,b) scanf("%lf%lf",&a,&b)
+#define mp(a,b) make_pair(a,b)
+using namespace std;
+typedef long long ll;
+int d(int x){
+    return 1;
+}
+int main(){
+    
+    return 0;
+}
